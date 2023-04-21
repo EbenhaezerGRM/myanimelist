af1ec6f41a727f16d14542cc0242371c0daca93d
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-04-21 13:13:28 +0700

    Add not found page

---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,18 @@
+import NavigationBar from "../components/NavigationBar"
+import NotFound from "../components/NotFound"
+
+const NoPage = () =>{
+    return(
+        <>
+        <NavigationBar/>
+        <br/>
+        <br/>
+        <br/>
+        <h1>
+            <NotFound/>
+        </h1>
+        </>
+    )
+}
+
+export default NoPage
