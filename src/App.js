35ebc9fdb55d29ef18ef12e8f16f7dbc1547376d
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-04-21 13:08:57 +0700

    New path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./style/StylingPage.css"
+import "./style/StylingComponents.css"
+
+import HomePage from "./pages/HomePage";
+import NoPage from "./pages/NoPage";
+import RandomPage from "./pages/RandomPage";
+
+const App = () => {
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/random" element={<RandomPage/>} />
+        <Route path="*" element={<NoPage/>} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
