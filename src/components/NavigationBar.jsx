be9c481abb43125a4208b0b51eda0787d65a4f68
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-04-25 11:13:33 +0700

    Add Anime and Manga navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,6 +3,8 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/logo/logo64.webp'
 import { useNavigate } from 'react-router-dom';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function NavigationBar() {
   const navigate = useNavigate()
@@ -20,9 +22,22 @@ function NavigationBar() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto ">
+          <Nav className="me-auto">
             <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
-            <Nav.Link onClick={() => navigate('/upcoming')}>Up Coming</Nav.Link>
+            <NavDropdown title="Anime" id="collasible-nav-dropdown">
+              <NavDropdown.Item onClick={() => navigate('/anime')}>Top Anime</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => navigate('/anime/season')}>Season</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Search</NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="Manga" id="collasible-nav-dropdown">
+              <NavDropdown.Item onClick={() => navigate('/')}>Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Something</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
           <Nav>
             <Nav.Link onClick={() => navigate('/random')}>Random</Nav.Link>
