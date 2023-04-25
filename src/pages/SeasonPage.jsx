4ed520d2a795a01d637dc9927a758d645e9e353e
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-04-25 11:13:44 +0700

    Add season

---
+++ src/pages/SeasonPage.jsx
@@ -0,0 +1,15 @@
+import NavigationBar from "../components/NavigationBar"
+import Season from "../components/Season"
+
+const SeasonPage = () => {
+    return(
+        <div className="seasonPage">
+            <NavigationBar/>
+            <br/>
+            <br/>
+            <Season/>
+        </div>
+    )   
+}
+
+export default SeasonPage
