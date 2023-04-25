ae1dc2a6b4ee924488baf5d55be1579693507b5a
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-04-25 11:13:55 +0700

    Anime page

---
+++ src/pages/AnimePage.jsx
@@ -0,0 +1,16 @@
+import NavigationBar from "../components/NavigationBar"
+import AnimeNavigationBar from "../components/AnimeNavigationBar"
+
+const AnimePage = () => {
+    return(
+        <div className="animePage">
+            <NavigationBar/>
+            <br/>
+            <br/>
+            <br/>
+            <AnimeNavigationBar/>
+        </div>
+    )   
+}
+
+export default AnimePage
