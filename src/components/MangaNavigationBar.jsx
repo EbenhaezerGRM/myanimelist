ce22cec32370973b6f1ec6bd85df7013e7f24847
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-04-25 18:56:47 +0700

    Add manga navbar

---
+++ src/components/MangaNavigationBar.jsx
@@ -0,0 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { useNavigate } from 'react-router-dom';
+
+function MangaNavigationBar() {
+  const navigate = useNavigate()
+  return (
+    <>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto ">
+            <Nav.Link onClick={() => navigate('/manga')}>All Manga</Nav.Link>
+            <Nav.Link onClick={() => navigate('/manga/top')}>Manga</Nav.Link>
+            <Nav.Link onClick={() => navigate('/manga/oneshot')}>Oneshot</Nav.Link>
+            <Nav.Link onClick={() => navigate('/manga/doujin')}>Doujinshi</Nav.Link>
+            <Nav.Link onClick={() => navigate('/manga/lightnovel')}>Light Novel</Nav.Link>
+            <Nav.Link onClick={() => navigate('/manga/novel')}>Novel</Nav.Link>
+            <Nav.Link onClick={() => navigate('/manga/manhwa')}>Manhwa</Nav.Link>
+            <Nav.Link onClick={() => navigate('/manga/manhua')}>Manhua</Nav.Link>
+            <Nav.Link onClick={() => navigate('/manga/popular')}>Popular</Nav.Link>
+            <Nav.Link onClick={() => navigate('/manga/favorite')}>Favorite</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+    </>
+  );
+}
+
+export default MangaNavigationBar;
