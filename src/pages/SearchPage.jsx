d55a270ba391da0813933bef2e5e4f5c6f3ec07b
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-04-27 09:53:54 +0700

    Add search page

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,15 @@
+import NavigationBar from "../components/NavigationBar"
+
+const SearchPage = () => {
+    return(
+        <div className="">
+            <NavigationBar/>
+            <br/>
+            <br/>
+            <br/>
+            Search Page
+        </div>
+    )   
+}
+
+export default SearchPage
