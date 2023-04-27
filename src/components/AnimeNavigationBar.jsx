74dccbd0c3f37745d4428e18d26fc48bf928d145
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-04-27 12:54:27 +0700

    Fix Navigation Bar

--- src/components/AnimeNavigationBar.jsx
+++ src/components/AnimeNavigationBar.jsx
@@ -4,29 +4,30 @@ import Nav from 'react-bootstrap/Nav';
 import { useNavigate } from 'react-router-dom';
 
 function AnimeNavigationBar() {
-  const navigate = useNavigate()
+const navigate = useNavigate()
+
   return (
-    <>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="border animeNavBar">
+    <div className="animeNavBar">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto ">
-            <Nav.Link onClick={() => navigate('/anime')}>All Anime</Nav.Link>
-            <Nav.Link onClick={() => navigate('/anime/airing')}>Airing</Nav.Link>
-            <Nav.Link onClick={() => navigate('/anime/upcoming')}>Up Coming</Nav.Link>
-            <Nav.Link onClick={() => navigate('/anime/tv')}>Tv</Nav.Link>
-            <Nav.Link onClick={() => navigate('/anime/movie')}>Movie</Nav.Link>
-            <Nav.Link onClick={() => navigate('/anime/ova')}>OVA</Nav.Link>
-            <Nav.Link onClick={() => navigate('/anime/ona')}>ONA</Nav.Link>
-            <Nav.Link onClick={() => navigate('/anime/special')}>Special</Nav.Link>
-            <Nav.Link onClick={() => navigate('/anime/popular')}>Popular</Nav.Link>
-            <Nav.Link onClick={() => navigate('/anime/favorite')}>Favorite</Nav.Link>
+            <Nav className="me-auto">
+                <Nav.Link onClick={() => navigate('/anime')}>All Anime</Nav.Link>
+                <Nav.Link onClick={() => navigate('/anime/airing')}>Airing</Nav.Link>
+                <Nav.Link onClick={() => navigate('/anime/upcoming')}>Up Coming</Nav.Link>
+                <Nav.Link onClick={() => navigate('/anime/tv')}>Tv</Nav.Link>
+                <Nav.Link onClick={() => navigate('/anime/movie')}>Movie</Nav.Link>
+                <Nav.Link onClick={() => navigate('/anime/ova')}>OVA</Nav.Link>
+                <Nav.Link onClick={() => navigate('/anime/ona')}>ONA</Nav.Link>
+                <Nav.Link onClick={() => navigate('/anime/special')}>Special</Nav.Link>
+                <Nav.Link onClick={() => navigate('/anime/popular')}>Popular</Nav.Link>
+                <Nav.Link onClick={() => navigate('/anime/favorite')}>Favorite</Nav.Link>
             </Nav>
             </Navbar.Collapse>
-        </Container>
+        </Container> 
         </Navbar>
-    </>
+    </div>
   );
 }
 
