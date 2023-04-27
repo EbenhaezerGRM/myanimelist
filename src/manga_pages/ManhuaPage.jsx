e9f2c1887950f5a6fb690292af65402c6157f222
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-04-27 12:56:58 +0700

    Fix Navigation Bar

--- src/manga_pages/ManhuaPage.jsx
+++ src/manga_pages/ManhuaPage.jsx
@@ -6,9 +6,6 @@ const ManhuaPage = () => {
     return(
         <div className="manhuaPage">
             <NavigationBar/>
-            <br/>
-            <br/>
-            <br/>
             <MangaNavigationBar/>
             <Manhua/>
         </div>
